4983c7950f1cf739eb528f6243004251e501df92
Gabriel Mohňanský <mohnanskygabriel@gmail.com>
2018-01-24 19:04:43 +0100

    Added Application class to demonstrate the work with the DAO

---
+++ src/main/java/Application.java
@@ -0,0 +1,20 @@
+/*import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import eventagent.persistence.dao.mysql.MySQLEventsSourceDAO;*/
+
+
+
+//this class is only demonstrating the usage of this persistence layer
+public class Application {
+	
+	/*
+	public static void main(String[] args) {
+		//initialize the Spring Application context
+		ApplicationContext context = new ClassPathXmlApplicationContext("MySQLPersistenceBeans.xml");
+		//get the dao defined in Bean
+		MySQLEventsSourceDAO dao = (MySQLEventsSourceDAO) context.getBean("eventsSourceDAO");
+		//work with dao as you want
+		System.out.println(dao.getAllsourceURLs().toString());
+	}*/
+
+}
